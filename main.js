46e9df511b6c10aa4c80ee2e083b8386b1f9801f
Eze Ochonogor <sendezeamail@gmail.com>
2021-03-13 11:06:39 +0100

    NV1

--- main.js
+++ main.js
@@ -1,15 +1,16 @@
-// gsap.registerPlugin(scrollTrigger)
-// gsap.registerPlugin(textPlugin)
-gsap.from('.iam_text', {opacity:0, scale:0.2, duration:5})
+document.querySelector('footer div:nth-child(2)').addEventListener('click', (e)=>window.scrollTo({top:0,left:0, behaviour:'smooth'}))
+
+gsap.registerPlugin("scrollTrigger")
+gsap.registerPlugin("textPlugin")
+
+const textTimeline = gsap.timeline()
+
+textTimeline.to('.iam_iam', {text: "Hello, I am Eze, <br />", ease:Power2.easeOut, duration:3})
+.from('.iam_fullstack', {opacity:0, ease:Bounce.easeOut, duration:2} )
+.to('.iam_welcome', {text: "Welcome to my world!!!", ease:Power4.easeOut, duration:3, delay:3} )
 
 
+gsap.from('.icodein_box', {duration:5, opacity:0, ease:Bounce.Out} )
+gsap.from('.iworkwith_box', {opacity:0, duration:5, ease:Linear} )
 
 
-//mobile_nav manipulations
-document.querySelector('.mobile_menu').addEventListener('click', (e)=>{
-    document.querySelector('div.hidden_nav').classList.toggle('mobile_nav')
-})
-
-document.querySelector('.close').addEventListener('click', (e)=>{
-    document.querySelector('.mobile_nav').classList.toggle('mobile_nav')
-})
