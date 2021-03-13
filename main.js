300799d495a3ae00616eb15ab3481e1dde15fe8e
Eze Ochonogor <sendezeamail@gmail.com>
2021-03-13 12:08:17 +0100

    nv5

--- main.js
+++ main.js
@@ -7,7 +7,7 @@ const textTimeline = gsap.timeline()
 
 textTimeline.to('.iam_iam', {text: "Hello, I am Eze, <br />", ease:Power2.easeOut, duration:3})
 .from('.iam_fullstack', {opacity:0, ease:Bounce.easeOut, duration:2} )
-.to('.iam_welcome', {text: "Welcome to my world!!!", ease:Power4.easeOut, duration:3, delay:3} )
+.to('.iam_welcome', {text: "Welcome to my world!!!", ease:Power4.easeOut, duration:3, delay:1} )
 
 
 gsap.from('.icodein_box', {duration:5, opacity:0, ease:Bounce.Out} )
